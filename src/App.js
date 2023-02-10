577ea188efded39c9ed8412e5bb64c965fbc4061
erdifree <123300447+erdifree@users.noreply.github.com>
2023-02-10 12:55:39 +0100

    api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div>Tanto ho vinto io!!</div>;
-}
+import { connection } from "./api";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const update = async () => {
+    const response = await connection();
+
+    setData(response);
+    console.log(response);
+  };
+
+  useEffect(() => {
+    update();
+  },[]);
+
+  return (
+    <div>
+      <h1>Marco Pagaaaa</h1>
+    </div>
+  );
+};
 
 export default App;
